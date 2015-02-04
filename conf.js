961fa85c19a87c7c34a533e436f607695f47feb9
Pranav Parikh <pranavp@yahoo-inc.com>
2015-02-04 13:48:22 -0800

    Initial commit for Protractor Saucelabs example

---
+++ conf.js
@@ -0,0 +1,25 @@
+// An example configuration file.
+exports.config = {
+
+    // This works fine, but looking for way if we can pass sauceUser and sauceKey as part of Selenium address
+    // like the way it works for Java - https://docs.saucelabs.com/tutorials/java/#junit
+//    sauceUser: process.env.SAUCE_USERNAME,
+//    sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['spec.js'],
+
+    // Is this supported ?
+    seleniumAddress: "http://sauceUer:sauceKey@ondemand.saucelabs.com:80/wd/hub",
+
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    }
+};
---
+++ spec.js
@@ -0,0 +1,7 @@
+describe('Yahoo homepage', function() {
+    it('should dummy test', function() {
+        browser.get('https://angularjs.org');
+
+        expect(1).toEqual(1);
+    });
+});
